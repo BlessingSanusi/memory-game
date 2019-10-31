83cabaf99512375815b72802aee307aec49cecfd
Blessing Sanusi <sanusi.temitope@ymail.com>
2019-10-31 11:17:56 -0500

    updating files

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -6,24 +6,25 @@ function Navbar(props) {
             <div className="navbar-header">
         
 					
-					<div class="business-logo-align col-md-10" >
+					<div class="business-logo-align col-md-12" >
 						<h3><a href="/">Memory Game</a></h3>
 					</div> 
 					
             </div>
 
             <div class="navbar-collapse collapse">
-					<ul class="nav navbar-nav navbar-right">
-						<li class="col-md-6 col-xs-6 text-center"><h5>Current Score: {props.score}</h5></li>
-						<li class="col-md-6 col-xs-6 text-center"><h5>Top Score: {props.topScore}</h5></li> 
-					</ul>
-					
-					<div class=" col-xs-9 col-sm-12 col-md-8 pd-t-10" >
+            <div class=" col-xs-9 col-sm-12 col-md-8 pd-t-10" >
 						<ul class="nav navbar-nav  navbar-centerr">
 							<li><h5>{props.message}</h5></li>
 							
 						</ul>
-					</div> 
+					</div>
+					<ul class="nav navbar-nav navbar-right">
+						<li class="col-md-8 col-xs-6 text-center"><h5>Current Score: {props.score}</h5></li>
+						<li class="col-md-8 col-xs-6 text-center"><h5>Top Score: {props.topScore}</h5></li> 
+					</ul>
+					
+					 
 				</div>
      
       </nav>
