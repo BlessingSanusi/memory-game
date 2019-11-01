1d8f47191157a471482f3796de4e319c02d0b01e
Blessing Sanusi <sanusi.temitope@ymail.com>
2019-10-31 21:49:40 -0500

    Updates

---
+++ precache-manifest.996cc86ceb1eed22bba22db5e8531b33.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbe0d2d2bb6d20bc8ca57c6fdc49a0b0",
+    "url": "/memory-game/index.html"
+  },
+  {
+    "revision": "15e486fe67e33927ce1e",
+    "url": "/memory-game/static/css/main.46621b61.chunk.css"
+  },
+  {
+    "revision": "98ed8dc2276602c45349",
+    "url": "/memory-game/static/js/2.3f7fed2f.chunk.js"
+  },
+  {
+    "revision": "15e486fe67e33927ce1e",
+    "url": "/memory-game/static/js/main.a06f4283.chunk.js"
+  },
+  {
+    "revision": "e24248335f986e44ed3f",
+    "url": "/memory-game/static/js/runtime-main.baf69392.js"
+  }
+]);
